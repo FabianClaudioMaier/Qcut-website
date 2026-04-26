7f1f801d6b5b28a7606bf07401056f0ace378455
nilsant <nils.antropow@gmail.com>
2026-04-26 13:33:51 +0200

    accept viewing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["qcut-website-production.up.railway.app"]
+  }
 });
