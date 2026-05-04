49dcc3808a52ced660303939f476b552e444b723
Fabian Maier <a11907564@univie.ac.at>
2026-05-04 09:09:04 +0200

    cookie declaration added

--- src/pages/PrivacyNoticePage.jsx
+++ src/pages/PrivacyNoticePage.jsx
@@ -4,6 +4,7 @@ function PrivacyNoticePage() {
   return (
     <main className="mx-auto w-full max-w-[1100px] px-4 pb-12 pt-8">
       <H1Block>privacy notice</H1Block>
+      <script id="CookieDeclaration" src="https://consent.cookiebot.com/2a475210-b203-4225-953a-af0cec101a9f/cd.js" type="text/javascript" async></script>
     </main>
   );
 }
